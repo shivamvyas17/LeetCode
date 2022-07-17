9c5b54e0f9e686fa4ff1feb6c4c90b31c5398c7b
SHIVAM VYAS <74703617+shivamvyas17@users.noreply.github.com>
2022-07-17 13:00:31 +0530

    Time: 1 ms (73.27%), Space: 51.7 MB (17.48%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -1,29 +1,16 @@
 class Solution {
     public int missingNumber(int[] nums) {
         
-     insersionSort(nums);
-for(int i=0; i< nums.length;i++){
-if(nums[i] != i){
-return i;
-}
-}
-return nums.length;
-}
-int [] insersionSort(int [] nums){
-int i= 0;
-while ( i< nums.length){
-if(nums[i] != i){
-if(nums[i] != (nums.length)){
-int temp = nums[nums[i]];
-nums[nums[i]] = nums[i];
-nums[i] = temp;
-}else{i++;
-continue;
-}
-}else{
-i++;
-}
-}
-return nums;
-}
+    int n=nums.length;
+    int totals=n *(n+1)/2;
+    int exp=0;
+      
+        for(int i=0;i<n;i++){
+            
+            exp=exp+nums[i];
+        }
+        return totals-exp;
+        
+    }
+    
 }
