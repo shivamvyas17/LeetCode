253e9a9f33c61eb6095898c01ed38b5cb65adefa
SHIVAM VYAS <74703617+shivamvyas17@users.noreply.github.com>
2022-07-21 22:10:37 +0530

    Time: 13 ms (5.92%), Space: 43.5 MB (5.52%) - LeetHub

--- 709-to-lower-case/709-to-lower-case.java
+++ 709-to-lower-case/709-to-lower-case.java
@@ -1,6 +1,13 @@
 class Solution {
     public String toLowerCase(String s) {
         
-   return s.toLowerCase();
+     String res="";
+        for(char i:s.toCharArray()){
+            if(i>='A'&& i<='Z'){
+                res+=(char)(i+32);
+            }
+            else{res+=i;}
+        }
+        return res;
     }
 }
