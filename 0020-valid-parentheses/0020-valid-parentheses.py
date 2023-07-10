0016cc88f544450c39ce654d759bda0e3c0a042c
SHIVAM VYAS <74703617+shivamvyas17@users.noreply.github.com>
2023-07-10 09:32:50 +0530

    Time: 51 ms (35.39%), Space: 16.3 MB (89.57%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,21 +1,33 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack=[]
-        if(len(s)==1):
-            return False
-        for ch in s:
-            if(ch =='(' or ch =='{'or ch =='[' ):
-                 stack.append(ch)
+        # if(len(s)==1):
+        #     return False
+        # for ch in s:
+        #     if(ch =='(' or ch =='{'or ch =='[' ):
+        #          stack.append(ch)
+        #     else:
+        #         if(ch == ')'):
+        #             if(len(stack)==0 or stack.pop()!='('):
+        #                 return False
+        #         if(ch == '}'):
+        #             if(len(stack)==0 or stack.pop()!='{'):
+        #                 return False
+        #         if(ch == ']'):
+        #             if(len(stack)==0 or stack.pop()!='['):
+        #                 return False
+        # return len(stack)==0
+        
+        dic = { ")":"(" ,"}":"{","]":"[" }
+        
+        for char in s:
+            if char in dic:
+                if stack and stack[-1]==dic[char]:
+                    stack.pop()
+                else:
+                    return False
             else:
-                if(ch == ')'):
-                    if(len(stack)==0 or stack.pop()!='('):
-                        return False
-                if(ch == '}'):
-                    if(len(stack)==0 or stack.pop()!='{'):
-                        return False
-                if(ch == ']'):
-                    if(len(stack)==0 or stack.pop()!='['):
-                        return False
-        return len(stack)==0
+                stack.append(char)
+        return True if not stack else False
     
         
